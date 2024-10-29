d448008b6b24d6a15563daf8ca6d496cedf4438b
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-10-29 12:39:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/PowerDaxx/SHUKLA-MUSIC-V1",
+    "https://github.com/PowerDaxx/SHUKLA-MUSIC-V",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
